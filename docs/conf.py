0ed1f19fd9c2c7239851a8db69886021682d58eb
ignazio-ingenito <ignazio@baialupo.com>
2020-12-01 11:45:29 +0000

    Update conf.py
    localisation of theme strings enables.

--- docs/conf.py
+++ docs/conf.py
@@ -1,4 +1,14 @@
 from recommonmark.parser import CommonMarkParser
+import sphinx_rtd_theme
+
+# Adding this theme as an extension is what enables localization of theme 
+# strings in your translated output. If these strings are not translated 
+# in your output, either we lack the localized strings for your locale, 
+# or you are using an old version of the theme.
+extensions = [
+    'sphinx_rtd_theme',
+]
+html_theme = "sphinx_rtd_theme"
 
 source_parsers = {
     '.md': CommonMarkParser,
